03a22e6a6354474529ce258632735f8b16a4941c
yingying.t@beryl8.com <yingying.t@beryl8.com>
2024-08-06 09:54:33 +0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import { Server } from './websocket/server';
 
 console.log('Starting service.');
+console.log('Test Add Git');
 
 dotenv.config();
 new Server().start();
