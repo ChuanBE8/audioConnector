3fb771888a38d3ab9d9b906f5a9ede37a4e6c64d
yingying.t@beryl8.com <yingying.t@beryl8.com>
2024-08-27 16:04:49 +0700

    no message

--- src/services/tts-service.ts
+++ src/services/tts-service.ts
@@ -34,9 +34,9 @@ export class TTSService {
             },
             audioConfig: {
                 audioEncoding: 'MULAW', // รูปแบบของไฟล์เสียงที่ต้องการ (MP3, OGG_OPUS, LINEAR16, etc.)
-                speakingRate: 1.2,      //0.25-4
-                pitch: -2,              //-20.0-20.0
-                volumeGainDb: 5,        //-96.0-16.0
+                speakingRate: 1.0,      //0.25-4
+                pitch: 0,              //-20.0-20.0
+                volumeGainDb: 0,        //-96.0-16.0
                 sampleRateHertz: 8000,
                 effectsProfileId: ['telephony-class-application'] //telephony-class-application, handset-class-device, headphone-class-device
             },
